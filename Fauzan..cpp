ed18d014d8e04af61fb3bc52519eb47395f52863
MFauzanD1mas <149571419+MFauzanD1mas@users.noreply.github.com>
2023-11-01 15:34:52 +0700

    Add files via upload

---
+++ Fauzan..cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int baris,kolom,atas;
+	
+	cout <<"Masukan jumlah kolom : ";
+	cin>>kolom;
+	
+	cout <<"Masukan jumlah garis : ";
+	cin>>baris;
+	
+	cout<<"Masukan jumlah penutup : ";
+	cin>>atas;
+		for(int C= 1; C<=baris; C++){
+		cout<<" __ ";
+	}
+	cout<<endl;
+	
+	for(int A= 1; A<=baris; A++){
+		for(int B= 1; B<=kolom; B++){
+			cout<<"|__|";
+		}		
+		cout<<endl;
+	}	
+
+	return 0;
+}
+
